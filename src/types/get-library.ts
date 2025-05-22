bdef7d437cfcd08ca46afdb4452ca59ee8c6d366
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-05-22 11:48:26 +0200

    feat: library sync

--- src/types/get-library.ts
+++ src/types/get-library.ts
@@ -1,22 +1,65 @@
+export interface LibraryItem {
+  id: string;
+  namespace: string;
+  title: string;
+  description: string;
+  effectiveDate: string;
+  keyImages: Array<{
+    type: string;
+    url: string;
+  }>;
+  seller: {
+    id: string;
+    name: string;
+  };
+  productSlug: string;
+  urlSlug: string;
+  url: string;
+  tags: Array<{
+    id: string;
+  }>;
+  items: Array<{
+    id: string;
+    namespace: string;
+  }>;
+  customAttributes: Array<{
+    key: string;
+    value: string;
+  }>;
+  categories: Array<{
+    path: string;
+  }>;
+  catalogNs: {
+    mappings: Array<{
+      pageSlug: string;
+      pageType: string;
+    }>;
+  };
+}
+
 export interface LibraryResponse {
-    responseMetadata: ResponseMetadata
-    records: Record[]
-  }
+  items: LibraryItem[];
+  paging: {
+    count: number;
+    start: number;
+    total: number;
+  };
+}
   
-  export interface ResponseMetadata {
-    nextCursor: string
-    stateToken: string
-  }
+export interface ResponseMetadata {
+  nextCursor: string
+  stateToken: string
+}
   
-  export interface Record {
-    namespace: string
-    catalogItemId: string
-    appName: string
-    productId: string
-    sandboxName: string
-    sandboxType: string
-    recordType: string
-    acquisitionDate: string
-    dependencies: any[]
-  }
+export interface Record {
+  namespace: string
+  catalogItemId: string
+  appName: string
+  productId: string
+  sandboxName: string
+  sandboxType: string
+  recordType: string
+  acquisitionDate: string
+  dependencies: any[]
+}
   
